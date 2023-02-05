5f18d4999e0b23a00b5303bd1eee981bc6cc637e
Brandon Martino <brandonmmartino1@gmail.com>
2023-02-05 16:27:27 -0600

    completed js

--- app.js
+++ app.js
@@ -26,3 +26,23 @@ document.addEventListener("click", function(e) {
         })
     }
 })
+
+// on submit, switch hidden class between rating card and thank you card
+const submitBtn = document.getElementById("submit");
+const thankYou = document.querySelector(".thanks");
+const ratingCard = document.querySelector(".rating");
+
+submitBtn.addEventListener("click", function(e) {
+    thankYou.classList.remove("hidden");
+    ratingCard.classList.add("hidden");
+})
+
+// parse through rating values and insert selection on thank you card
+const result = document.querySelector(".target")
+
+ratingBtn.forEach(btn => {
+    btn.addEventListener("click", function() {
+        const selectedRating = this.value;
+        result.textContent = `You selected ${selectedRating} out of 5`;
+    })
+})
